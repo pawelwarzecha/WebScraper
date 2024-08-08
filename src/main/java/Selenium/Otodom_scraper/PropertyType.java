b9e26bb78b325dbea3c6798fc09ce1e49dc72f17
pawelwarzecha <pawelwarzecha99@gmail.com>
2024-08-08 20:38:23 +0200

    added comment

--- src/main/java/Selenium/Otodom_scraper/PropertyType.java
+++ src/main/java/Selenium/Otodom_scraper/PropertyType.java
@@ -3,6 +3,9 @@ package Selenium.Otodom_scraper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+//change the number in data.properties to select different type, 0 = apartment, 1 = studio apartment, 2 = house,
+// 3 = investments, 4 = rooms, 5 = plot, 6 = business premises, 7 = warehouses, 8 = garages
+
 public class PropertyType {
 
     public static void selectType(WebDriver driver, String type){
